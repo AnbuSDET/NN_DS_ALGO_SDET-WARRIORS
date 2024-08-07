5f157a3c96d9294b38f14740ae7c2c90c53c9e78
vijayashree <mathustudies@gmail.com>
2024-08-06 21:34:24 -0500

    TestNG converted update by Viji

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,11 +1,8 @@
 package testRunner;
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
 @CucumberOptions (
 			  features={".//FeatureFiles/GetStarted.feature"},
 			  glue = "stepDefinitions",
@@ -24,6 +21,6 @@ import io.cucumber.junit.CucumberOptions;
 			  
 		)
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
