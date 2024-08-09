4ca5e1185c31a17dc71fd40535e1e5a5bf182a3f
[AnbuSDET] <anbumay25@gmail.com>
2024-08-08 21:52:59 -0400

    Commit by Anbu

--- src/test/java/pageObjects/DataStructurePage.java
+++ src/test/java/pageObjects/DataStructurePage.java
@@ -1,5 +1,14 @@
 package pageObjects;
 
-public class DataStructurePage {
+import org.openqa.selenium.WebDriver;
+
+public class DataStructurePage extends BasePage{
+
+	public DataStructurePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
