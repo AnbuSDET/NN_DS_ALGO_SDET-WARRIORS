36a73a0cbe5feb5e217d48947bb548f69ba22384
[AnbuSDET] <anbumay25@gmail.com>
2024-08-12 12:50:10 -0400

    Commit by Anbu

--- src/test/java/stepDefinitions/QueueSteps.java
+++ src/test/java/stepDefinitions/QueueSteps.java
@@ -1,48 +1,82 @@
 package stepDefinitions;
 
+import java.util.Properties;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import Factory.BaseClass;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pageObjects.GetStartedPage;
+import pageObjects.HomePage;
+import pageObjects.QueuePage;
+import pageObjects.SignInPage;
 
 public class QueueSteps {	
+	
+	WebDriver driver;
+	Properties p;
+	Logger logger = BaseClass.getLogger();
+	JavascriptExecutor jse = (JavascriptExecutor)BaseClass.getDriver();
 	
 	
 	@Given("The user is logged in to the application with Valid credentials")
 	public void the_user_is_logged_in_to_the_application_with_valid_credentials() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		logger.info("Login to test the Queue Page Functionalities.........");		
+	    GetStartedPage gsp = new GetStartedPage(BaseClass.getDriver());
+		gsp.clickGetStartedSP();
+		HomePage hp =new HomePage(BaseClass.getDriver());
+		hp.clickSigIn();	
+		logger.info("User login with valid username and password..........");
+		SignInPage sp = new SignInPage(BaseClass.getDriver());
+	    sp.enterUsername("Testing");
+		 sp.enterpassword("Password@143");
+		 sp.clickLogin();
 	}
 
 	@When("The User clicks the Queue_Get Started button in the Home page")
 	public void the_user_clicks_the_queue_get_started_button_in_the_home_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		logger.info("Clicking the Queue Page After Login..........");			
+		jse.executeScript("window.scrollBy(0,500)"); 
+		HomePage hp = new HomePage(BaseClass.getDriver());	
+		hp.clickQueueGetStarted();
+		
 	}
 
 	@Then("The user should be able to navigate to the Queue Page")
 	public void the_user_should_be_able_to_navigate_to_the_queue_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    String CurntURL= BaseClass.getDriver().getCurrentUrl();
+	    Assert.assertEquals(CurntURL, CurntURL.contains("queue"));
+		logger.info("Queue Page URL --->" + CurntURL);
 	}
 	
 	   // Implementation of Queue Page
 	
 	@Given("The user is in the Queue Page")
 	public void the_user_is_in_the_queue_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   
+     logger.info("The user is in the Queue Page");
+     String CurntURL= BaseClass.getDriver().getCurrentUrl();
+     Assert.assertEquals(CurntURL, CurntURL.contains("queue"));
 	}
 
 	@When("The User clicks the Implementation of Queue in Python link  in the Queue Page")
 	public void the_user_clicks_the_implementation_of_queue_in_python_link_in_the_queue_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		QueuePage Qp = new QueuePage (BaseClass.getDriver());
+		Qp.Click_ImplementaionOfQueue();
+		
 	}
 
 	@Then("The user should be navigate to the Implementation of Queue in Python Page")
 	public void the_user_should_be_navigate_to_the_implementation_of_queue_in_python_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   
+		String CrtURL = BaseClass.getDriver().getCurrentUrl();
+		Assert.assertEquals(CrtURL, CrtURL.contains(""));
 	}
 	
 	//Implementation using collections
@@ -50,20 +84,17 @@ public class QueueSteps {
 	
 	@Given("The user is in the Queue Page after logged in")
 	public void the_user_is_in_the_queue_page_after_logged_in() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   
 	}
 
 	@When("The User clicks the  Implementation using collections queue link in Queue Page")
 	public void the_user_clicks_the_implementation_using_collections_queue_link_in_queue_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("The user should be navigate to the Implementation using collections queue Page")
 	public void the_user_should_be_navigate_to_the_implementation_using_collections_queue_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 	
 	
