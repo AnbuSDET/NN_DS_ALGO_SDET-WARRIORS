433a2c8ddd5ed8e46eab860284bd035305b4739f
[AnbuSDET] <anbumay25@gmail.com>
2024-08-13 15:56:50 -0400

    Conflict Resolving

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,35 +4,34 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
-		//features={".//FeatureFiles/"},
-	 	  //features={".//FeatureFiles/Home.feature"},
-	      //features={".//FeatureFiles/GetStarted.feature"
-	      //features ={".//FeatureFiles/SignIn.feature"},
-	      //features ={".//FeatureFiles/Register.feature"},
-		  //features={".//FeatureFiles/DataStructure.feature"},
-	      //features={".//FeatureFiles/Array.feature"},
-        //features={".//FeatureFiles/LinkedList.feature"},
-	      //features={".//FeatureFiles/Stack.feature"},
-	      //features={".//FeatureFiles/Queue.feature"},
-	 	  //features={".//FeatureFiles/Tree.feature"},
-		  //features={".//FeatureFiles/Graph.feature"},
-		  
-		  glue = "stepDefinitions",
-		  plugin = {"pretty", "html:reports/myreport.html", 
-				  "rerun:target/rerun.txt",
-				  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-				},
-		  dryRun = false,
-		  monochrome = true,
-		  publish = true
-		  //tags="@sanity"  // this will execute scenarios tagged with @sanity
-		  //tags="@Trial"
-		  //tags="@sanity and @regression" //Scenarios tagged with both @sanity and @regression
-		  //tags="@sanity and not @regression" //Scenarios tagged with @sanity but not tagged with @regression
-		  //tags="@sanity or @regression" //Scenarios tagged with either @sanity or @regression	
-		  
-	)
-
+	       	   //features={".//FeatureFiles/"},
+		 	  //features={".//FeatureFiles/Home.feature"},
+		      //features={".//FeatureFiles/GetStarted.feature"
+		      //features ={".//FeatureFiles/SignIn.feature"},
+		      //features ={".//FeatureFiles/Register.feature"},
+			  //features={".//FeatureFiles/DataStructure.feature"},
+		      //features={".//FeatureFiles/Array.feature"},
+	          //features={".//FeatureFiles/LinkedList.feature"},
+		      //features={".//FeatureFiles/Stack.feature"},
+		      //features={".//FeatureFiles/Queue.feature"},
+		 	  //features={".//FeatureFiles/Tree.feature"},
+			  //features={".//FeatureFiles/Graph.feature"},
+			  
+			  glue = "stepDefinitions",
+			  plugin = {"pretty", "html:reports/myreport.html", 
+					  "rerun:target/rerun.txt",
+					  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+					},
+			  dryRun = false,
+			  monochrome = true,
+			  publish = true
+			  //tags="@sanity"  // this will execute scenarios tagged with @sanity
+			  //tags="@Trial"
+			  //tags="@sanity and @regression" //Scenarios tagged with both @sanity and @regression
+			  //tags="@sanity and not @regression" //Scenarios tagged with @sanity but not tagged with @regression
+			  //tags="@sanity or @regression" //Scenarios tagged with either @sanity or @regression	
+			  
+		)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
