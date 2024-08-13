883e41619a79f9091ea9e524d681cb6055c01eb7
[AnbuSDET] <anbumay25@gmail.com>
2024-08-13 11:08:16 -0400

    Queue Module Completed

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
 			  // features={".//FeatureFiles/GetStarted.feature"},
-		      features={".//FeatureFiles/Home.feature"},
+		      features={".//FeatureFiles/Queue.feature"},
 		      //features={".//FeatureFiles/SignIn.feature"},
 		      //features={".//FeatureFiles/Register.feature"},
 			  glue = "stepDefinitions",
@@ -17,7 +17,7 @@ import io.cucumber.testng.CucumberOptions;
 			  monochrome = true,
 			  publish = true
 			  //tags="@sanity"  // this will execute scenarios tagged with @sanity
-			  //tags="@regression"
+			 // tags="@test"
 			  //tags="@sanity and @regression" //Scenarios tagged with both @sanity and @regression
 			  //tags="@sanity and not @regression" //Scenarios tagged with @sanity but not tagged with @regression
 			  //tags="@sanity or @regression" //Scenarios tagged with either @sanity or @regression	
