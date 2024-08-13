14486564a63b3832abe859e1e34590b6ae40649e
Mathura <mathustudies@gmail.com>
2024-08-13 11:19:59 -0400

    Stack commit mathura

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
-			  // features={".//FeatureFiles/GetStarted.feature"},
-		      features={".//FeatureFiles/Home.feature"},
+				 // features={".//FeatureFiles/GetStarted.feature"},
+		      //features={".//FeatureFiles/Home.feature"},
 		      //features={".//FeatureFiles/SignIn.feature"},
 		      //features={".//FeatureFiles/Register.feature"},
 			  glue = "stepDefinitions",
